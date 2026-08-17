11760552927e3b2fd5d7d9d65ba7e2291a342514
威晨 劉 <week861011@gmail.com>
2026-08-18 02:54:52 +0800

    行程規劃功能 list 、 create 、view  建置完成，待除錯

---
+++ prjMvcCore第四組/Controllers/TripController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using prjMvcCore第四組.Models;
+using prjMvcCore第四組.ViewModels;
+using prjMvcCore第四組.Models;
+
+namespace prjMvcCore第四組.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(CRestaurantViewModel vm)
+        {
+            MidprjDb2Context db = new MidprjDb2Context();
+            var datas = db.TTrips.Include(t => t.FUsers).ToList();
+            DateTime now = DateTime.Now;
+            bool isUpdated = false;
+            List<CTripWrap> list = new List<CTripWrap>();
+            foreach (var black in datas)
+            {
+                CTripWrap blue = new CTripWrap() { Trip = black };
+                if (blue.StatusEnum != CTripWrap.TripStatus.已結束 && blue.FCreatedTime.AddSeconds(30) <= now)
+                {
+                    blue.StatusEnum = CTripWrap.TripStatus.已結束;
+                    blue.Trip.FUpdatedTime = now;
+                    isUpdated = true;
+                }
+
+                list.Add(blue);
+            }
+
+            if (isUpdated)
+            {
+                db.SaveChanges();
+            }
+
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            MidprjDb2Context db = new MidprjDb2Context();
+            CTripWrap cTrip = new CTripWrap();
+            cTrip.Restaurants = db.TRestaurants.Select(t => new SelectListItem
+            {
+                Value = t.FRestaurantId.ToString(),
+                Text = t.FName
+            }).ToList();
+            return View(cTrip);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CTripWrap cTrip, string submitButton)
+        {
+            MidprjDb2Context db = new MidprjDb2Context();
+
+            if (submitButton == "儲存為草稿")
+            {
+                cTrip.StatusEnum = CTripWrap.TripStatus.草稿;
+            }
+            else if (submitButton == "新增行程")
+            {
+                cTrip.StatusEnum = CTripWrap.TripStatus.已完成;
+            }
+
+            cTrip.Trip.FCreatedTime = DateTime.Now;
+
+            db.TTrips.Add(cTrip.Trip);
+            db.SaveChanges();
+            if (cTrip.SelectedRestaurantIds != null && cTrip.SelectedRestaurantIds.Count > 0)
+            {
+                int sortOrder = 1;
+                foreach (var restId in cTrip.SelectedRestaurantIds)
+                {
+                    var tripRest = new TTripRestaurant
+                    {
+                        FTripId = cTrip.Trip.FTripId,
+                        FRestaurantId = restId,
+                        FSortOrder = sortOrder++,
+                        FCreatedTime = DateTime.Now
+                    };
+                    db.TTripRestaurants.Add(tripRest);
+                }
+                db.SaveChanges();
+            }
+            cTrip.Restaurants = db.TRestaurants
+            .Select(r => new SelectListItem
+            {
+                Value = r.FRestaurantId.ToString(),
+                Text = r.FName
+            }).ToList();
+
+
+            return RedirectToAction("List");
+
+        }
+
+
+
+    }
+}
+
---
+++ prjMvcCore第四組/Models/CTripWrap.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using prjMvcCore第四組.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace prjMvcCore第四組.Models
+{
+    public class CTripWrap
+    {
+        public enum TripStatus
+        {
+            草稿 = 0,
+            已完成 = 1,
+            已結束 = 2
+        }
+
+        private TTrip _trip;
+        
+        public TTrip Trip {
+            get { return _trip; }
+            set { _trip = value; }
+        }
+        public CTripWrap() 
+        {
+            _trip = new TTrip();
+        }
+
+        [Key]
+        public long FTripId { get; set; }
+
+        public int FUsersId { get; set; }
+        [DisplayName("行程名稱")]
+        public string FTripName { get; set; } = null!;
+        [DisplayName("行程日期")]
+        public DateOnly FTripDate { get; set; }
+        [DisplayName("開始時間")]
+        public TimeOnly? FStartTime { get; set; }
+        [DisplayName("行程描述")]
+        public string? FDescription { get; set; }
+        [DisplayName("行程規劃狀態")]
+        public string FStatus { get; set; } = null!;
+        [DisplayName("新增日期")]
+        public DateTime FCreatedTime { get; set; }
+        [DisplayName("最後更新日期")]
+        public DateTime? FUpdatedTime { get; set; }
+
+        public virtual TUser FUsers { get; set; } 
+
+        public virtual ICollection<TTripRestaurant> TTripRestaurants { get; set; } = new List<TTripRestaurant>();
+
+        public TripStatus StatusEnum
+        {
+            get
+            {
+                if (Enum.TryParse<TripStatus>(FStatus, out var result))
+                {
+                    return result;
+                }
+                return TripStatus.草稿; // 預設值
+            }
+            set
+            {
+                FStatus = value.ToString(); // 自動將 Enum 名稱 (如 "草稿") 轉成字串存入 FStatus
+            }
+        }
+
+        public IEnumerable<SelectListItem> StatusSelectList
+        {
+            get
+            {
+                return Enum.GetValues(typeof(TripStatus))
+                           .Cast<TripStatus>()
+                           .Select(e => new SelectListItem
+                           {
+                               Text = e.ToString(),
+                               Value = e.ToString(),
+                               Selected = (e.ToString() == FStatus) // 自動選取當前狀態
+                           });
+            }
+        }
+
+        public List<SelectListItem> Restaurants { get; set; } = new List<SelectListItem>();
+        // 接收前端傳回來的餐廳選擇結果 (按排序順序傳回 RestaurantID)
+        public List<long> SelectedRestaurantIds { get; set; } = new List<long>();
+    }
+}

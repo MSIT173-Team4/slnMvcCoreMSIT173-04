a8f324751bb1430e2e140868e25cb0b36091906e
k147324 <10156115@gm.scu.edu.tw>
2026-08-17 11:38:42 +0800

    新增member controller 跟index view

---
+++ prjMvcCore第四組/Controllers/MemberController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjMvcCore第四組.Models;
+
+namespace prjMvcCore第四組.Controllers
+{
+    public class MemberController : Controller
+{
+    public IActionResult Index()
+    {
+        MidprjDb2Context db=new MidprjDb2Context();
+            var data = from t in db.TUsers select t;
+        return View(data);
+    }
+    public IActionResult Create()
+    {
+        return View();
+    }
+        [HttpPost]
+        public IActionResult Create(TUser u)
+        {
+            MidprjDb2Context db = new MidprjDb2Context();
+
+            db.Add(u);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    public IActionResult Details(int? id)
+     {
+            if (id == null) return RedirectToAction("Index");
+            MidprjDb2Context db = new MidprjDb2Context();
+            TUser data = db.TUsers.FirstOrDefault(t => t.FId == id);
+            TPostTable pdata = db.TPostTables.FirstOrDefault(t => t.FUserId == id);
+            TRecipe rdata = db.TRecipes.FirstOrDefault(t => t.FAuthorUserId == id);
+            if (data == null||pdata==null||rdata==null) return RedirectToAction("Index");
+            ViewBag.user = data;
+            ViewBag.post = pdata;
+            ViewBag.recipe = rdata;
+            return View();
+    }
+
+
+}
+}
